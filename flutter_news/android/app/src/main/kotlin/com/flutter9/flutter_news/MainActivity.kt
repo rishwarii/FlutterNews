0b413a41879c48bceb7edbfc981fa1fade9df23b
rishh <rihwariranjan@gmail.com>
2021-01-24 00:00:25 +0530

    init2

--- flutter_news/android/app/src/main/kotlin/com/flutter9/flutter_news/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.flutter9.flutter_news
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ flutter_newws/android/app/src/main/kotlin/com/flutter9/flutter_newws/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter9.flutter_newws
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
